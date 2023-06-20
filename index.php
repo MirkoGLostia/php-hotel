7dc6160b708050bfc2d24a581f54c15fa6eb04a7
MirkoGLostia <giggi.torre@mail.com>
2023-06-20 15:45:45 +0200

    inserimento dati in tabella

--- index.php
+++ index.php
@@ -10,57 +10,85 @@
 </head>
 <body>
 
+    <?php
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    
+    ];
+    ?>
+
     <header>
         <!-- search bar -->
     </header>
 
-    <main>
+    <main class="container">
         <!-- tabella con elenco di hotel -->
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance from center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach ($hotels as $hotel) {
+                        echo "<tr>";
+
+                        echo "<th>" . $hotel["name"] . "</th>";
+                
+                        echo "<td>" . $hotel["description"] . "</td>";
+                
+                        echo "<td>" . $hotel["parking"] . "</td>";
+                
+                        echo "<td>" . $hotel["vote"] . "</td>";
+                
+                        echo "<td>" . $hotel["distance_to_center"] . "</td>";
+
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
     </main>
 
-<?php
-
-$hotels = [
-
-    [
-        'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
-        'parking' => true,
-        'vote' => 4,
-        'distance_to_center' => 10.4
-    ],
-    [
-        'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 2
-    ],
-    [
-        'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
-        'parking' => false,
-        'vote' => 1,
-        'distance_to_center' => 1
-    ],
-    [
-        'name' => 'Hotel Bellavista',
-        'description' => 'Hotel Bellavista Descrizione',
-        'parking' => false,
-        'vote' => 5,
-        'distance_to_center' => 5.5
-    ],
-    [
-        'name' => 'Hotel Milano',
-        'description' => 'Hotel Milano Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 50
-    ],
-
-];
-
-
-?>
 </body>
 </html>
